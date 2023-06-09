65bae7cac9f3b8cf3e1ee5d0212a110ea4eca73e
Amauriane <115087575+Amauriane@users.noreply.github.com>
2023-06-09 16:11:13 -0300

    Update java.js

--- js/java.js
+++ js/java.js
@@ -5,6 +5,8 @@ function salvarCookie(event) {
         const formulario = {
             'Nome do formulário': 'Cadastro de Cliente',
             'Nome': document.getElementById('nome').value,
+            'Email': document.getElementById('email').value,
+            'Telefone': document.getElementById('telefone').value,
             'ID': document.getElementById('id').value,
             'Tipo de cliente': document.getElementById('tipoCliente').value,
             'Endereço': document.getElementById('endereco').value,
@@ -13,7 +15,30 @@ function salvarCookie(event) {
             'Data de Nascimento': document.getElementById('dataNascimento').value,
             'Vendedor': document.getElementById('vendedor').value,
             'Limite de crédito': document.getElementById('limiteCredito').value
-        };
+        }
+        const errorMessages = [];
+        if (!/^[a-zA-Z\s]+$/.test(nome)) {
+            errorMessages.push('O nome deve conter apenas letras.');
+        }
+
+        if (!id || isNaN(id)) {
+            errorMessages.push('O ID deve ser um número válido.');
+        }
+
+        if (isNaN(cep)) {
+            errorMessages.push('O CEP deve conter apenas números.');
+        }
+
+        if (isNaN(limiteCredito) || limiteCredito < 0) {
+            errorMessages.push('O limite de crédito deve ser um número válido e não negativo.');
+        }
+
+        if (errorMessages.length > 0) {
+            throw new Error(errorMessages.join('\n'));
+        }
+
+        document.cookie = `nome=${nome}`;
+        document.cookie = `id=${id}`;
 
         document.cookie = "formulario=" + JSON.stringify(formulario);
         alert("Cadastro realizado com sucesso!");
@@ -22,4 +47,10 @@ function salvarCookie(event) {
         console.error(error);
         alert("Erro no cadastro: " + error);
     }
+}
+function validateEmail(email) {
+    const atIndex = email.indexOf('@');
+    const dotIndex = email.lastIndexOf('.');
+
+    return atIndex > 0 && dotIndex > atIndex;
 }
